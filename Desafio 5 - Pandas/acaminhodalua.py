e52dd25d9db80468400f04baeda22dafbf36302b
chandrasantos <chandra2santos@gmail.com>
2023-11-14 01:32:20 -0300

    desafio 5

---
+++ Desafio 5 - Pandas/acaminhodalua.py
@@ -0,0 +1,17 @@
+# Explore o espaço com o Python e o Visual Studio Code; inspirado pelo filme da Netflix "A Caminho da Lua"
+
+""" Aprender a codificar às vezes parece estar fora do nosso alcance. Mas as pessoas que cruzaram a atmosfera, orbitaram ao redor da Terra ou andaram na lua nos mostraram que mesmo as metas mais distantes exigem determinação e dedicação. Neste roteiro de aprendizagem, veremos esses temas na história A Caminho da Lua.
+
+A Caminho da Lua é um filme sobre Fei Fei, uma garotinha que constrói um foguete para viajar até a lua em uma missão a fim de provar a existência de uma lendária deusa da lua. A garota é impulsionada pelas memórias e pelo amor da mãe a usar a determinação e a imaginação para conquistar algo que está além deste mundo: chegar à lua. Embora a história ocorra em um universo lindamente desenhado, ela está diretamente relacionada aos tipos de problemas que engenheiros reais enfrentam enquanto se preparam e executam missões para a lua e além dela.
+
+Os módulos foram inspirados por A Caminho da Lua e pela história dos engenheiros e astronautas reais da NASA. Reunindo os princípios de ciência de dados, aprendizado de máquina e inteligência artificial com ferramentas como o Python, o Visual Studio Code e o Azure, estes módulos orientam você sobre como obter novas habilidades em tecnologia e imaginar como resolver desafios relacionados a viagem espacial. Não se esqueça de assistir ao filme A Caminho da Lua na Netflix hoje!
+
+Nestes módulos, você vai aprender a:
+
+Agregar dados de várias fontes em DataFrames do pandas
+Usar Python para explorar, limpar e manipular dados
+Garantem que sejam aplicadas práticas éticas ao longo do desenvolvimento de IA
+Treinar um modelo de IA de Visão Personalizada sobre um animal específico """
+
+#Neste módulo, você vai se inspirar no filme A Caminho da Lua e nos cientistas da NASA para explorar dados relacionados às missões Apollo e às amostras de rochas que foram trazidas para a Terra. Você aprenderá técnicas de limpeza e de manipulação de dados que são essenciais para desenvolver os modelos de machine learning e IA que nos ajudam a nos prepararmos para a exploração espacial.
+
