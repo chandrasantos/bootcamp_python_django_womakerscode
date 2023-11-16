ec57aa0ba5aa6d6f2f751f2c41cf695c8ac5c11c
chandrasantos <chandra2santos@gmail.com>
2023-11-15 21:47:30 -0300

    Update 10_anotacoes_teoricas_django.py

--- 10_anotacoes_teoricas_django.py
+++ 10_anotacoes_teoricas_django.py
@@ -14,3 +14,15 @@
 #para criar o projeto criar django-admin startproject projeto_womakers . (o ponto final é importante para ele não criar duas pastas iguais) serve para configurações gerais do sistema, rotas etc.
 #python manage.py runserver (comando para rodar o sistema), se funcionou ele mostra um endereço local http.
 # python manage.py startapp base (é o aplicativo,funcionalidades a serem desenvolvidas)
+
+
+#AULAS
+#Github: SistemaCadastroCursos/aula_01
+#Github: SistemaCadastroCursos/aula_02
+#Github: SistemaCadastroCursos/aula_03
+#Github: SistemaCadastroCursos/aula_04
+#Github: SistemaCadastroCursos/aula_06
+#Github: SistemaCadastroCursos/aula_admin
+#Github: SistemaCadastroCursos/aula_validacoes
+#Github: SistemaCadastroCursos/aula_cache
+
