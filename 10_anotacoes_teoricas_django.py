d3df5d85b5f66688daaf715bb3701fca4f3bc13c
chandrasantos <chandra2santos@gmail.com>
2023-11-15 21:10:13 -0300

    Create 10_anotacoes_teoricas_django.py

---
+++ 10_anotacoes_teoricas_django.py
@@ -0,0 +1,16 @@
+# Framework e o padrão MTV
+
+#framework é uma caixa de ferramentas. Coleção de códigos prontos e bibliotecas ajudando a criar códigos.
+#MVT - é o padrão do Django - Model View Template (Front) Orinetada a template
+#MVC - Model View Controller (Laravel) Orientada a controller
+#django, montar páginas/templates, interagir com diversos bancos de dados/orm, validar input dos usuarios/forms, controlar acesso/authorization, gerenciar aplicacao/admin
+#instalar ambiente virtual: pip install virtualenv
+#criar projeto no ambiente virtual: pyyhon -m venv cadastro_curso_womakers // cadastro_curso_womakers é o nome do projeto
+#localizar  apasta cd - \cadastro_curso_womakers (confirmar esse cd no windows)
+#ativar: .\Scripts\activate
+#lembrar de desativar o ambiente virtual
+# pip install django (para instalar dentro da pasta)
+# os pacotes e bibliotecas ficam instalados localmente, ver na pasta LIB. 
+#para criar o projeto criar django-admin startproject projeto_womakers . (o ponto final é importante para ele não criar duas pastas iguais) serve para configurações gerais do sistema, rotas etc.
+#python manage.py runserver (comando para rodar o sistema), se funcionou ele mostra um endereço local http.
+# python manage.py startapp base (é o aplicativo,funcionalidades a serem desenvolvidas)
